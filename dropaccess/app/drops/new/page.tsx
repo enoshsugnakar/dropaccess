bd26f050b1a962df43b6a2e986370ca92a78095e
Enosh Sugnakar <111285971+enoshsugnakar@users.noreply.github.com>
2025-06-05 07:48:46 +0000

    fixed form page

--- dropaccess/app/drops/new/page.tsx
+++ dropaccess/app/drops/new/page.tsx
@@ -6,7 +6,7 @@ export default function NewDropPage() {
   return (
     <div className="min-h-screen bg-background py-8">
       <Navbar/>
-      <div className="container ml-20 max-w-auto mx-auto px-4">
+      <div className="container lg:ml-20 max-w-auto mx-auto px-4">
         <DropForm />
       </div>
     </div>
